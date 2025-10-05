47fcaa8420a4864b2e75874ccf8d174fa9d58167
hkjin <wlsgusrb78@naver.com>
2025-10-05 22:09:35 +0900

    :memo: add OpenAPI configurations and dependencies to service modules

---
+++ pub-sub-example/audit-service/src/main/java/io/github/Hyeonqz/audit/config/OpenAPIConfig.java
@@ -0,0 +1,24 @@
+package io.github.Hyeonqz.audit.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public Info info() {
+        return new Info()
+                .title("Audit Service API")
+                .version("1.0")
+                .description("감시 API 문서")
+                ;
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(info());
+    }
+}
